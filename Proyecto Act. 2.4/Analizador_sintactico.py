0c42bebbc7e72ffef84afc73b5b0bf7795999cd0
irving Ancheyta Castro <157397938+Iaac17@users.noreply.github.com>
2024-09-28 14:04:34 -0600

    Update Analizador_sintactico.py

--- Proyecto Act. 2.4/Analizador_sintactico.py
+++ Proyecto Act. 2.4/Analizador_sintactico.py
@@ -1,13 +1,14 @@
 import ply.yacc as yacc
-from Analizador_Lexico2 import tokens  # Importar los tokens desde el archivo lexico.py
+from Analizador_Lexico2 import tokens  
 
 # Resultado del análisis sintáctico
 resultado_gramatica = []
+total_lineas = 0  
 
 # Definir las reglas gramaticales
 def p_expresion(p):
     '''
-    expresion : RESERVADA DELIMITADOR RESERVADO IDENTIFICADOR OPERADOR ENTERO SIMBOLO IDENTIFICADOR MENORIGUAL ENTERO SIMBOLO IDENTIFICADOR PLUSPLUS DELIMITADOR DELIMITADOR RESERVADO SIMBOLO RESERVADO SIMBOLO RESERVADO DELIMITADOR COMDOB IDENTIFICADOR DOSPUNTO COMDOB SUMA IDENTIFICADOR DELIMITADOR SIMBOLO DELIMITADOR
+    expresion : RESERVADA DELIMITADOR RESERVADO IDENTIFICADOR OPERADOR ENTERO SIMBOLO IDENTIFICADOR OPERADOR ENTERO SIMBOLO IDENTIFICADOR PLUSPLUS DELIMITADOR DELIMITADOR RESERVADO SIMBOLO RESERVADO SIMBOLO RESERVADO DELIMITADOR COMDOB IDENTIFICADOR DOSPUNTO COMDOB SUMA IDENTIFICADOR DELIMITADOR SIMBOLO DELIMITADOR
               | RESERVADO SIMBOLO RESERVADO SIMBOLO RESERVADO DELIMITADOR COMDOB IDENTIFICADOR DOSPUNTO COMDOB SUMA IDENTIFICADOR DELIMITADOR SIMBOLO 
               | DELIMITADOR DELIMITADOR
     '''
@@ -18,23 +19,29 @@ def p_expresion(p):
     else:
         p[0] = "Expresión correcta (llave de cierre)"
 
-# Manejo de errores sintácticos
+# Manejo de errores sintácticos mejorado
 def p_error(p):
     global resultado_gramatica
+    global num_saltos_linea  
+
     if p:
-        resultado = f"Error sintáctico en el token {p.type} con valor {p.value}"
+        resultado = f"Error sintáctico en la línea {p.lineno-total_lineas}  posición {p.lexpos}: token inesperado '{p.value}'."
     else:
-        resultado = "Error sintáctico: código incompleto"
+        resultado = "Error sintáctico: código incompleto o inesperado al final."
+
     resultado_gramatica.append(resultado)
 
 # Inicializar el parser
 parser = yacc.yacc()
 
-# Función para realizar el análisis sintáctico
-def realizar_analisis_sintactico(code):
+def realizar_analisis_sintactico(code, total_lineas_param):
     global resultado_gramatica
+    global total_lineas
+
+    total_lineas = total_lineas_param 
     resultado_gramatica.clear()
     resultado_sintactico = parser.parse(code)
+
     if resultado_sintactico:
         resultado_gramatica.append(resultado_sintactico)
     return resultado_gramatica
