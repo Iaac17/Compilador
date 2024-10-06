9d22659238dec3a64dd895c4715374c21a6a686d
irving Ancheyta Castro <157397938+Iaac17@users.noreply.github.com>
2024-10-05 23:33:28 -0600

    Update Analizador_Lexico2.py

--- Act. 2.5/Analizador_Lexico2.py
+++ Act. 2.5/Analizador_Lexico2.py
@@ -1,19 +1,26 @@
 import ply.lex as lex
 from collections import defaultdict
+from collections import Counter
 
 # Definir los tokens para el analizador léxico
 tokens = (
-    'RESERVADA', 'RESERVADO', 'DELIMITADOR', 'SIMBOLO', 'OPERADOR', 
+    'RESERVADA', 'RESERVADO', 'SIMBOLO', 'OPERADOR', 
     'IDENTIFICADOR', 'ENTERO', 'PLUSPLUS',
-    'COMDOB', 'DOSPUNTO', 'SUMA', 'RESTA'
+    'COMDOB', 'DOSPUNTO', 'RESTA', 'COMA' 
+    ,'SUMA' , 'PARIZQ', 'PARDER', 'LLAIZQ', 'LLADER'
 )
 
 # Expresiones regulares para los tokens
+t_COMA = r'\,'
 t_PLUSPLUS = r'\+\+'
 t_RESTA = r'\-'
 t_COMDOB = r'"'
 t_DOSPUNTO = r':'
 t_SUMA = r'\+'
+t_PARIZQ = r'\('
+t_PARDER = r'\)'
+t_LLAIZQ = r'\{'
+t_LLADER = r'\}'
 
 # Definir tokens reservados
 def t_RESERVADA(t):    
@@ -24,16 +31,12 @@ def t_RESERVADO(t):
     r'(public|static|System|void|out|int|println|read|printf|end)'
     return t
 
-def t_DELIMITADOR(t):
-    r'\(|\)|\{|\}'
+def t_OPERADOR(t):
+    r'=|<=|>='
     return t
 
 def t_SIMBOLO(t):
     r';|\.'
-    return t
-
-def t_OPERADOR(t):
-    r'=|<=|>='
     return t
 
 def t_ENTERO(t):
@@ -64,9 +67,25 @@ def realizar_analisis_lexico(code):
     lexer.input(code)
     tokens_lexico = []
     category_count = defaultdict(int)  # Contador para las categorías
+    contador = Counter()
     lexer.lineno = 1  # Reiniciar el contador de líneas
 
     for tok in lexer:
+        if tok.type == 'RESERVADA':
+            contador['Reservada'] += 1
+        elif tok.type == 'RESERVADO':
+            contador['Reservado'] += 1
+        elif tok.type in ['SIMBOLO', 'COMA']:
+            contador['Simbolo'] += 1
+        elif tok.type == 'IDENTIFICADOR':
+            contador['Identificador'] += 1
+        elif tok.type == 'ENTERO':
+            contador['Numero'] += 1
+        elif tok.type in ['PARIZQ', 'PARDER', 'LLAIZQ', 'LLADER']:
+            contador['Delimitador'] += 1
+        else:
+            contador['Desconocido'] += 1
+
         # Registrar el tipo de token y aumentar el contador de esa categoría
         category_count[tok.type] += 1
         
@@ -79,7 +98,7 @@ def realizar_analisis_lexico(code):
         })        
     # Convertir el defaultdict a una lista de tuplas
     category_count_list = list(category_count.items())
-    
     total_lineas = lexer.lineno - 1 
     
-    return tokens_lexico, category_count_list, total_lineas 
+    return tokens_lexico, contador, category_count_list, total_lineas
+            
